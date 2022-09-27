d9875e1f7f496eb26741e8b7e36fde0c9c637201
Yannick Sutter <yannick.sutter@post.ch>
2022-09-27 11:47:12 +0200

    first draft

---
+++ ClearPasswordFinder/PasswordFinder.cs
@@ -0,0 +1,37 @@
+﻿namespace ClearPasswordFinder;
+
+public class PasswordFinder
+{
+    private static string[] Patterns = { "-P", "pwd", "password", "pass", "secret" };
+    // Process all files in the directory passed in, recurse on any directories
+    // that are found, and process the files they contain.
+    public static void ProcessDirectory(string targetDirectory)
+    {
+        // Process the list of files found in the directory.
+        string [] fileEntries = Directory.GetFiles(targetDirectory);
+        foreach(string fileName in fileEntries)
+            ProcessFile(fileName);
+
+        // Recurse into subdirectories of this directory.
+        string [] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+        foreach(string subdirectory in subdirectoryEntries)
+            ProcessDirectory(subdirectory);
+    }
+
+    // Insert logic for processing found files here.
+    public static void ProcessFile(string path)
+    {
+        var foundOccurence = false;
+        var linesOfWords = File.ReadLines(path).Select(item => item.Split(" ")).ToList();
+        linesOfWords.ForEach(line => line.ToList().ForEach(word =>
+        {
+            if (Patterns.Contains(word))
+            {
+                foundOccurence = true;
+                Console.WriteLine("found occurrence of possible password in: {0}", word);
+            }
+        }));
+        if(foundOccurence)
+            Console.WriteLine("Processed file '{0}' and found occurence: {1}.", path, foundOccurence);	
+    }
+}
